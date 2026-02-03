65ec45da72673aa07818ae36c4658890db0fea4d
ankitbh4tt <bhattankit152207@gmail.com>
2026-02-03 22:39:22 +0530

    fix the safeprovider bug

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { AppNavigator } from "./src/navigation/AppNavigator";
 import { useConfigStore } from "./src/config/store";
 import { View, ActivityIndicator } from "react-native";
 import { COLORS } from "./src/constants/theme";
+
+import Toast from "react-native-toast-message";
 
 export default function App() {
   const { fetchConfig, isLoading } = useConfigStore();
@@ -16,15 +18,17 @@ export default function App() {
 
   if (isLoading) {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }} edges={["top","bottom"]}>
         <ActivityIndicator size="large" color={COLORS.primary} />
-      </View>
+      </SafeAreaView>
+
     );
   }
 
   return (
     <SafeAreaProvider>
       <AppNavigator />
+      <Toast />
     </SafeAreaProvider>
   );
 }
